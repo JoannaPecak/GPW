f07a9a891aa16069231deb36acccc1393bf1c851
JoannaPecak <joanna.pecak@edu.uekat.pl>
2018-04-03 00:03:47 +0200

    first commit

---
+++ src/gpw/ServerDanychGieldowych.java
@@ -0,0 +1,9 @@
+package gpw;
+
+import java.util.Observer;
+
+public interface ServerDanychGieldowych {
+    void addObserver(Observer o);
+
+    void symulujTransakcje();
+}
---
+++ src/gpw/WyswietlaczTikow.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+/**
+ * @author User
+ *
+ */
+package gpw;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class WyswietlaczTikow implements Observer {
+
+    @Override
+    public void update(Observable o, Object arg) {
+        Tik tik = (Tik) arg;
+        System.out.println("WT:Spó³ka: " + tik.getNazwa() + " cena: " + tik.getCena());
+    }
+}
